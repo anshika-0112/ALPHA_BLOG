7c8fd10fd571e0537ede3dd0cf88359f3f844ebb
anshika0112 <anshikabansal13@gmail.com>
2022-01-15 17:03:24 +0530

    Added functionality to edit Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
         @article=Article.new
     end
 
+    def edit
+        @article=Article.find(params[:id])
+    end
+
     def create
         @article=Article.new(params.require(:article).permit(:title,:descrition,:author))
         respond_to do |format|
@@ -19,6 +23,19 @@ class ArticlesController < ApplicationController
                 redirect_to @article  #shorthand:- redirect_to @article
             else
                 format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @article.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def update
+        @article=Article.find(params[:id])
+        respond_to do |format|
+            if @article.update(params.require(:article).permit(:title,:descrition,:author))
+                flash[:notice]="Article edited successfully"
+                redirect_to @article
+            else
+                format.html { render :edit, status: :unprocessable_entity }
                 format.json { render json: @article.errors, status: :unprocessable_entity }
             end
         end
