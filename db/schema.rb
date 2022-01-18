8584573a9fc7de22d9fad26d9bc6cf6b94b098a6
anshika0112 <anshikabansal13@gmail.com>
2022-01-18 17:39:38 +0530

    Removed author column from articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_144650) do
+ActiveRecord::Schema.define(version: 2022_01_18_120341) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "descrition"
-    t.string "author"
     t.datetime "created_at", precision: 6
     t.datetime "updated_at", precision: 6
   end
