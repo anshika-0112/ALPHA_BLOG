a5acf576ea81a67ccdca006eba2d0c96d6c45bba
anshika0112 <anshikabansal13@gmail.com>
2022-01-21 22:44:28 +0530

    added-pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
     end
 
     def index
-        @users=User.all
+        @users=User.paginate(page: params[:page],per_page:5)
     end
 
     def show
-        @articles=@user.articles
+        @articles=@user.articles.paginate(page: params[:page],per_page:5)
     end
 
     def create
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     def update
         if @user.update(user_params)
             flash[:notice]="User information updated successfully"
-            redirect_to articles_path
+            redirect_to @user
         else
             render :edit, status: :unprocessable_entity
         end
