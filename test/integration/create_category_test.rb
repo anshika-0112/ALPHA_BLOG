3d28ef3bf713ac1605a0c41a6cc0828db154e654
anshika0112 <anshikabansal13@gmail.com>
2022-01-24 17:31:12 +0530

    Restricted non-admins to create a category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+    def setup
+        @admin_user= User.create(username:"yuvraj",email:"yuvraj@gmail.com",password:"password",admin:true)
+        sign_in_as(@admin_user)
+    end
+
     test "get new category form and create category" do
         get "/categories/new"
         assert_response :success
