039c68d43c24ebf69cf0783b8a6ebb2f48d1efd0
anshika0112 <anshikabansal13@gmail.com>
2022-01-24 16:20:08 +0530

    Added functionality to create category along with testing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,28 @@
 class CategoriesController <  ApplicationController
 
     def new
+        @category=Category.new
     end
 
     def index
     end
 
     def show
+    end
+
+    def create
+        @category=Category.new(category_params)
+        if @category.save
+            flash[:notice]="Category created successfully"
+            redirect_to category_path(@category)
+        else
+            render 'new',status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
